b55a34ce61d2ca7f96918d106668437371a745c1
pasman9710 <felixzailskas@gmail.com>
2021-11-02 19:12:43 +0100

    City Tiles act randomly

--- kits/python/simple/agent.py
+++ kits/python/simple/agent.py
@@ -6,7 +6,8 @@ from lux.game_map import Cell, RESOURCE_TYPES
 from lux.constants import Constants
 from lux.game_constants import GAME_CONSTANTS
 from lux import annotate
-from worker import take_worker_action
+from worker import get_worker_action
+from city_tiles import get_city_tile_action
 from tile_finding import *
 
 
@@ -42,7 +43,7 @@ def agent(observation, configuration):
         if unit.is_worker() and unit.can_act():
             # do worker logic
             # determine the move to execute
-            action = take_worker_action(player, opponent, unit, game_state, resource_tiles)
+            action = get_worker_action(player, opponent, unit, game_state, resource_tiles)
             if action:
                 actions.append(action)
 
@@ -51,10 +52,12 @@ def agent(observation, configuration):
             pass
 
     for city in player.cities.items():
-        # logging.info(f"Curr City: {city}")
         for city_tile in city[1].citytiles:
-            pass
-            # logging.info(f"City Tile: {city_tile}")
+            if not city_tile.can_act():
+                continue
+            action = get_city_tile_action(player, opponent, city_tile, game_state, resource_tiles)
+            if action:
+                actions.append(action)
 
     # you can add debug annotations using the functions in the annotate object
     # actions.append(annotate.circle(0, 0))
